eb1ca3d43cd9738583d420e252bf36a39d007fa7
Ayooluwa Aduwo <aduwoayooluwa@gmail.com>
2025-06-11 05:57:40 +0100

    [FIX] vercel issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Header } from './components/Header';
 import { WeatherCard } from './components/WeatherCard';
 import { WeeklyForecast } from './components/WeeklyForecast';
